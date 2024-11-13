0d78a4dc273d8cc07ece08bce4bc5912dc8ba28f
Hai Nam <hainam000090@gmail.com>
2024-11-13 09:35:56 +0700

    fix data type

--- Terraform-VPC/modules/ec2/main.tf
+++ Terraform-VPC/modules/ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
   count=length(var.ec2_names)
   ami           = data.aws_ami.amazon-2.id
   instance_type = "t2.micro"
-  vpc_security_group_ids = [var.sg_id]
+  vpc_security_group_ids = var.sg_id
   associate_public_ip_address = true
   subnet_id = var.subnets[count.index]
   user_data =  var.user_data
