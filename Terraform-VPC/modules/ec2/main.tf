dd9e5d3ff3d8065f221478a67f969302d6b508d2
Hai Nam <hainam000090@gmail.com>
2024-11-13 16:12:33 +0700

    kk

--- Terraform-VPC/modules/ec2/main.tf
+++ Terraform-VPC/modules/ec2/main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "web" {
   count=length(var.ec2_names)
   ami           = data.aws_ami.amazon-2.id
   instance_type = "t2.micro"
+  security_groups = [var.sg_id]
   #vpc_security_group_ids = [var.sg_id]
   vpc_security_group_ids    = [var.sg_id]
   associate_public_ip_address = true
--- Terraform-VPC/modules/sg/outputs.tf
+++ Terraform-VPC/modules/sg/outputs.tf
@@ -1,3 +1,7 @@
 output "sg_id" {
   value = aws_security_group.sg.id
 }
+
+output "test" {
+  value = aws_security_group.sg.id  
+}
