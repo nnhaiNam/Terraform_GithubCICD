68fcd0e8e274d179c9629d844bfc2f7cd70f40ef
Hai Nam <hainam000090@gmail.com>
2024-11-13 16:23:01 +0700

    fixxx nnn

--- Terraform-VPC/main.tf
+++ Terraform-VPC/main.tf
@@ -20,6 +20,32 @@ module "ec2" {
 
 }
 
+resource "aws_instance" "test" {
+  
+  ami           = "ami-0fff1b9a61dec8a5f"
+  instance_type = "t2.micro"
+  
+  #vpc_security_group_ids = [var.sg_id]
+  vpc_security_group_ids    = [module.sg.sg_id]
+  associate_public_ip_address = true
+  subnet_id = module.vpc.subnet_ids[0]
+ 
+  availability_zone = "us-east-1a"
+  key_name = var.key_name  
+  
+
+  monitoring      = true 
+
+  metadata_options {
+    http_tokens = "required"  # Forces the use of IMDSv2 (IMDSv1 will be disabled)
+    http_endpoint = "enabled" # Enables the Instance Metadata Service
+  }
+
+  tags = {
+    Name = "EC2 test"
+  }
+}
+
 module "alb" {
   source = "./modules/alb"
   sg_id = module.sg.sg_id
